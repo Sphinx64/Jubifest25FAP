35cc91248f0cca3cf7aed385569de59a6b4781e8
grafpia <grafpia@gmail.com>
2025-09-04 17:48:12 +0000

    sec

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// WICHTIG: Ersetze die folgenden Platzhalter mit deinen echten Werten aus der Firebase Konsole!
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTbLCLBADWVRWbC9vpA6jJ0CN01ANZPAk",
-  authDomain: "anmeldungjubilaeumsfest-2025.firebaseapp.com",
-  projectId: "anmeldungjubilaeumsfest-2025",
-  storageBucket: "anmeldungjubilaeumsfest-2025.firebasestorage.app",
-  messagingSenderId: "766583458685",
-  appId: "1:766583458685:web:e5d37784976e3e858ada14",
-  measurementId: "G-DC6CC90H4R"
-};
+    apiKey: "AIzaSyBTbLCLBADWVRWbC9vpA6j...", // Ersetzen Sie dies mit Ihrem echten Firebase API Key
+    authDomain: "fap-jubilaeum-25.firebaseapp.com",
+    projectId: "fap-jubilaeum-25",
+    storageBucket: "fap-jubilaeum-25.appspot.com",
+    messagingSenderId: "1088458369388",
+    appId: "1:1088458369388:web:e47c9df1a3e7244955216f"
+  };
