0a99664995694a04ea48ed307183340fbe957124
grafpia <grafpia@gmail.com>
2025-09-04 21:03:45 +0000

    r

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTbLCLBADWVRWbC9vpA6jJ0CN01ANZPAk", // Ersetzen Sie dies mit Ihrem echten Firebase API Key
+    authDomain: "fap-jubilaeum-25.firebaseapp.com",
+    projectId: "fap-jubilaeum-25",
+    storageBucket: "fap-jubilaeum-25.appspot.com",
+    messagingSenderId: "1088458369388",
+    appId: "1:1088458369388:web:e47c9df1a3e7244955216f"
+  };
