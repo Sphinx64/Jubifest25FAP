35600d815779277ca33d516de59703bbb3877583
grafpia <grafpia@gmail.com>
2025-09-05 14:03:21 +0000

    sheets

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # Using unstable to get a newer firebase-tools version
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
@@ -23,40 +23,5 @@
     extensions = [
       # "vscodevim.vim"
     ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #       PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Zeigt eine Willkommensnachricht im Terminal an
-        welcome-message = ''
-            echo "Firebase-Entwicklungsumgebung ist startklar."
-            echo "Du kannst jetzt 'firebase emulators:start' oder 'firebase deploy' verwenden."
-        '';
-      };
-    };
   };
 }
-
