d98ba008291a863949c153c67f27749450ef96ef
grafpia <grafpia@gmail.com>
2025-09-12 10:15:52 +0000

    update daten

--- functions/event-config.js
+++ functions/event-config.js
@@ -16,9 +16,9 @@ const eventConfig = {
     // === DATUM UND ZEIT ===
     datetime: {
         date: "Samstag, 18. Oktober 2025",
-        shortDate: "18.10.2025", 
+        shortDate: "18.10.2025",
         startTime: "14:00 Uhr",
-        endTime: "22:00 Uhr",
+        endTime: "22:00 Uhr", // Ende des offiziellen Teils, gemütliches Beisammensein geht länger
         // Für .ics Kalender (UTC Format)
         icsStart: "20251018T120000Z", // 14:00 CEST = 12:00 UTC
         icsEnd: "20251018T200000Z"    // 22:00 CEST = 20:00 UTC
@@ -38,12 +38,11 @@ const eventConfig = {
 
     // === ANMELDUNG ===
     registration: {
-        deadline: "Dienstag, 9. September 2025",
-        shortDeadline: "09.09.2025",
-        // ✅ KORRIGIERT: GitHub Pages URL (deine aktuelle URL)
+        // ✅ AKTUALISIERT
+        deadline: "Sonntag, 28. September 2025",
+        shortDeadline: "28.09.2025",
         url: "https://sphinx64.github.io/Jubifest25FAP",
         password: "Agility",
-        // ✅ KORRIGIERT: QR-Code mit GitHub Pages URL
         qrCodeUrl: "https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=https://sphinx64.github.io/Jubifest25FAP/"
     },
 
@@ -52,19 +51,23 @@ const eventConfig = {
         members: "kostenlos",
         membersNote: "Mitglieder sind eingeladen",
         adultsPrice: "40 CHF",
-        childrenPrice: "15 CHF", 
+        childrenPrice: "15 CHF",
         childrenAgeLimit: "bis 14 Jahre",
         paymentMethods: "Bar oder Twint vor Ort"
     },
 
     // === PROGRAMM ===
+    // ✅ AKTUALISIERT
     program: [
-        { time: "14:00 Uhr", activity: "Offizielle Begrüssung und Apéro" },
-        { time: "15:00 Uhr", activity: "Gemeinsamer Spaziergang" },
-        { time: "17:00 Uhr", activity: "Jubiläums-Quizz" },
-        { time: "18:00 Uhr", activity: "Fondue oder Raclette" },
-        { time: "Anschliessend", activity: "Dessertbuffet, Drinks und Musik" }
+        { time: "14:00 Uhr", activity: "Apéro" },
+        { time: "15:00 Uhr", activity: "Spaziergang mit Hunden (evtl. mit Posten)" },
+        { time: "17:00 Uhr", activity: "Vereinsquiz" },
+        { time: "18:00 Uhr", activity: "Nachtessen" },
+        { time: "20:00 Uhr", activity: "Offizieller Teil (Worte, Video, Infos zum Jubi.)" },
+        { time: "20:45 Uhr", activity: "Dessert" },
+        { time: "21:45 Uhr", activity: "Gemütliches Beisammensein mit Musik & Barbetrieb" }
     ],
+
 
     // === KONTAKT ===
     contact: {
@@ -106,3 +109,4 @@ if (typeof module !== 'undefined' && module.exports) {
 if (typeof window !== 'undefined') {
     window.eventConfig = eventConfig;
 }
+
